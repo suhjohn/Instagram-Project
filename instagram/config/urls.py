7a75cf19507812d3a0c1e7e5a59b045245c42457
John Sangwon Suh <johnsuh94@gmail.com>
2017-10-13 16:56:29 +0900

    post_list 기초 구현

--- instagram/config/urls.py
+++ instagram/config/urls.py
@@ -18,8 +18,11 @@ from django.conf.urls import url
 from django.conf.urls.static import static
 from django.contrib import admin
 
+from post.views import post_list
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', post_list, name='post_list')
 ]
 urlpatterns += static(
     settings.MEDIA_URL,
--- instagram/post/views.py
+++ instagram/post/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+from post.models import Post
+
+
+def post_list(request):
+    """
+    모든 Post목록을 리턴
+    template은 'post/post_list.html'사용
+    :param request:
+    :return:
+    """
+
+    posts = Post.objects.all()
+    context = {
+        'posts':posts,
+    }
+    return render(request, 'post/post_list.html', context)
